9af2817757f0d2f4caf351cd418d1d7a3bbee400
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-06-04 14:26:53 -0300

    tratamento de erro, despesa

--- app/src/main/java/com/alves/organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/alves/organizze/activity/DespesasActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.alves.organizze.R;
+import com.alves.organizze.config.ConfiguracaoFirebase;
 import com.alves.organizze.helper.DateUtil;
 import com.alves.organizze.model.Movimentacao;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private EditText campoData, campoCategoria, campoDescricao, campoValor;
     private Movimentacao movimentacao;
+    private DatabaseReference firebaseref = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +55,36 @@ public class DespesasActivity extends AppCompatActivity {
     }
 
     public Boolean validarCampoDespesa(){
+        String textoValor = campoValor.getText().toString();
+        String textoData = campoData.getText().toString();
+        String textoCategoria = campoCategoria.getText().toString();
+        String textoDescricao = campoDescricao.getText().toString();
 
+        if ( !textoValor.isEmpty() ){
+            if ( !textoData.isEmpty() ){
+                if ( !textoCategoria.isEmpty() ){
+                    if ( !textoDescricao.isEmpty() ){
+                        return true;
+                    }else{
+                        Toast.makeText(DespesasActivity.this, "Descrição não foi preenchido!", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }else{
+                    Toast.makeText(DespesasActivity.this, "Categoria não foi preenchido!", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }else{
+                Toast.makeText(DespesasActivity.this, "Data não foi preenchido!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }else{
+            Toast.makeText(DespesasActivity.this, "Valor não foi preenchido!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
 
+    }
 
-        return true;
+    public void recuperarDespesatotal(){
+        
     }
 }
