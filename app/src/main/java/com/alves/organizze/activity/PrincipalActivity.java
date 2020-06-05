006f765bf33bd052421f58c9b748efc57608faaf
Danilo Medeiros Dantas <danilomedeiros.dox@gmail.com>
2020-06-05 11:38:57 -0300

    update

--- app/src/main/java/com/alves/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/alves/organizze/activity/PrincipalActivity.java
@@ -3,16 +3,22 @@ package com.alves.organizze.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.alves.organizze.config.ConfiguracaoFirebase;
+import com.alves.organizze.helper.Base64Custom;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.alves.organizze.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -21,6 +27,8 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView calendarView;
     private TextView textoSaudacao, textoSaldo;
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
 
 
     @Override
@@ -28,6 +36,7 @@ public class PrincipalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
 
         textoSaldo = findViewById(R.id.textSaldo);
@@ -46,6 +55,34 @@ public class PrincipalActivity extends AppCompatActivity {
             }
         });*/
 
+
+    }
+
+    public void recuperarResumo(){
+        String emailUsuario= autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+
+        DatabaseReference usuarioRef = firebaseRef.child("usuarios").child(idUsuario);
+    }
+
+    //Aula 198
+    //Criando menu na toolbar
+    public boolean onCreateOptionsMenu( Menu menu ){
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu( menu );
+    }
+
+    public boolean onOptionsItemSelected( MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                autenticacao.signOut();
+                startActivity( new Intent(this, MainActivity.class));
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected( item );
 
     }
 
